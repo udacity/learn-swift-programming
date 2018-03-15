6140887e8db1bb93b7cec5e0a1b5e9264c96b0f6
Owen LaRosa <owen.larosa@outlook.com>
2018-03-15 15:59:13 -0400

    Updates to variables/constants/literals

---
+++ Variables and Types.playground/Pages/Naming Variables and Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [Previous](@previous)
+//: ## Naming Variables and Constants
+//: Valid names
+let firstName = "Gabrielle"
+let _founder = "Sebastian"
+let 🐢 = "turtle"
+//: Invalid names
+//3rdFromTheSun = "🌎"
+//var $amount = 31
+//: Unclear names
+let hw = 13.50 // hours worked
+let rh = 40 // regular hours
+let ot = 5 // overtime
+//: Better Descriptive names
+let hourlyWage = 13.50
+let regularHours = 40
+let overtime = 5
+//: ## Literals
+//: `5 and `"hello"` are literals
+var myFavoriteNumber = 5
+var salutation = "hello"
+//: `salutation` is not a literal
+var greeting = salutation
+
+//: [Next](@next)
--- Variables and Types.playground/Pages/Tips With String Literals.xcplaygroundpage/Contents.swift
+++ Variables and Types.playground/Pages/Tips With String Literals.xcplaygroundpage/Contents.swift
@@ -1,10 +1,10 @@
 //: [Previous](@previous)
 //: ## Tips With String Literals
 //: escape characters
-//let emailSignature = "John Smith
+//let emailSignature = "Aliyah Smith
 //VP, Sales
 //(555) - 555 - 5555"
-let emailSignature = "John Smith\nVP, Sales\n(555) - 555 - 5555"
+let emailSignature = "Aliyah Smith\nVP, Sales\n(555) - 555 - 5555"
 //: string interpolation
 var name = "Kate"
 var customizedBirthdayCheer = "Happy Birthday, \(name)!"
